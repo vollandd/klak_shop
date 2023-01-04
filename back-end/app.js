17a3904096aa3359046625c52c03155590c6662d
nassim <linnnux@hotmail.fr>
2023-01-04 16:28:07 +0100

    :v 0.2.0 : structure refactoring : add controller & routes folders

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-
-const app = express();
 
 require('dotenv').config();
-
+const express  = require('express');
+const app      = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const mongoose = require('mongoose');
+
+const productRoutes = require('./routes/productRouter');
+
+
 
 mongoose.connect(process.env.ID_BDD,
 { useNewUrlParser: true,
@@ -19,114 +23,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
 
-app.use('/api/shoes', (req, res, next) => {
-    const shoes = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Geox',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2014/10/27/19/18/baby-shoes-505471_960_720.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Kechua',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2016/12/10/16/57/shoes-1897708_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeoghth',
-        title: 'Abidas',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2016/03/27/22/16/fashion-1284496_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeoghth',
-        title: 'Rmax',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2014/01/22/19/38/boot-250012_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
 
-    res.status(200).json(shoes);
-    next();
-  });
-
-  app.use('/api/clothing', (req, res, next) => {
-    const clothing = [
-      {
-        _id: 'jjjjjjjj',
-        title: 'cravatte',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2013/11/14/12/34/neckties-210347_960_720.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'gtthghgddf',
-        title: 'hiver',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2017/11/23/03/17/christmas-2971961_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'jldlfkggd',
-        title: 'jeans',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeoghth',
-        title: 'doudoune',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2018/03/01/14/57/portrait-3190849_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    
-    res.status(200).json(clothing);
-    next();
-  });
-
-  const Product = require('./models/product');
-
-  app.use('/api/add', (req, res, next) => {
-    const product = new Product({
-        title: 'doudoune',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2018/03/01/14/57/portrait-3190849_960_720.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      });
-    product.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-      next();
-  });
-
-  app.use('/api/products', (req, res, next) => {
-    Product.find()
-      .then(products => res.status(200).json(products))
-      .catch(error => res.status(400).json({ error }));
-      next();
-  });
-
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
+app.use('/api/',productRoutes);
 
 module.exports = app;
