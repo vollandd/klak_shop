e735972674eed44561b0fa5b8217784fda01830c
nassim <linnnux@hotmail.fr>
2023-01-09 12:55:34 +0100

    :v 0.2.4 : add uploads/img folder

---
+++ back-end/middleware/pictureFileManagerMiddleWare.js
@@ -0,0 +1,8 @@
+/*
+  to do
+    1 - install & require npm
+    2 - select mime types
+    3 - configure picture file storage destination
+    4 - configure picture file name
+    5 - export module
+*/
